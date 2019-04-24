e4a14268c901393e05bb8eb02ac4cc2d0a3568ca
katherine <rusnakkat@gmail.com>
2019-04-23 19:04:53 -0700

    learning number data types

---
+++ js/number.js
@@ -0,0 +1,38 @@
+// Number data type
+var a = 10;
+console.log(a);
+
+var b = 100;
+console.log(b);
+
+var a = 20;
+console.log(a);
+
+var c = 0x1E; //ascii hex value of 30
+console.log(c);
+
+var d = 0.5;
+console.log(d);
+
+//addition
+var sum = a + b;
+console.log(sum);
+
+//subtraction
+var diff = d - c;
+console.log(diff);
+
+//remainder
+var mod = c % b;
+console.log(mod);
+
+//division
+var div = a / d;
+console.log(div);
+
+//multiplication
+var mult = a * d;
+console.log(mult);
+
+var e = '5'; //outputs letter five not digit 5
+console.log(e); //(typeof e)->returns type of var'string'
